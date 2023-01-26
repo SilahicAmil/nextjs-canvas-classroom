0f1c798d2fe14d0bb9a9bd6b8b494cff891f14cd
Amil Silahic <jinobie02@gmail.com>
2023-01-26 09:52:32 -0700

    adding an pop up drawer when clicking on avatar

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,35 +1,51 @@
 import { Avatar } from "@boringer-avatars/react";
 import { CgBell } from "react-icons/cg";
+import { useState } from "react";
 
 const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openAccountSettingsHandler = () => {
+    setIsOpen((prevState) => !prevState);
+  };
+
   return (
-    <nav className="h-16 w-[100vw] font-mono text-xl">
-      <ul className="flex items-center justify-evenly h-full w-full ">
-        <li>
-          <div>
-            <h1>SCHOLAR</h1>
+    <>
+      <nav className="h-16 w-[100vw] font-mono text-xl">
+        <ul className="flex items-center justify-evenly h-full w-full ">
+          <li>
+            <div>
+              <h1>SCHOLAR</h1>
+            </div>
+          </li>
+          <li>Dashboard</li>
+          <li>Calendar</li>
+          <div className="flex gap-6 items-center">
+            {/* onClick Account should show dropdown of like settings, help, and etc */}
+            <li onClick={openAccountSettingsHandler}>
+              <Avatar
+                title={false}
+                size={40}
+                variant="beam"
+                name="testing"
+                square={false}
+                colors={["#FFAD08", "#EDD75A", "#73B06F", "#0C8F8F", "#405059"]}
+              />
+            </li>
+            {isOpen ? (
+              <div className="absolute flex flex-col justify-center items-center gap-4 h-48 rounded-lg w-48 mt-64 z-10 bg-slate-200">
+                <li>Settings</li>
+                <li>Help</li>
+                <li>Logout</li>
+              </div>
+            ) : undefined}
+            <li>
+              <CgBell className="text-xl" />
+            </li>
           </div>
-        </li>
-        <li>Dashboard</li>
-        <li>Calendar</li>
-        <div className="flex gap-6 items-center">
-          {/* onClick Account should show dropdown of like settings, help, and etc */}
-          <li>
-            <Avatar
-              title={false}
-              size={40}
-              variant="beam"
-              name="testing"
-              square={false}
-              colors={["#FFAD08", "#EDD75A", "#73B06F", "#0C8F8F", "#405059"]}
-            />
-          </li>
-          <li>
-            <CgBell className="text-xl" />
-          </li>
-        </div>
-      </ul>
-    </nav>
+        </ul>
+      </nav>
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ const HomePage = ({}) => {
   return (
     <>
       <div>
-        <h1 className="text-3xl bg-red-500">Hello</h1>
+        <h1 className="text-3xl bg-red-500"></h1>
       </div>
     </>
   );
