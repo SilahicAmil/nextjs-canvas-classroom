00523e4a9c432c7020c6a1612dd8fc781464c26f
Amil Silahic <jinobie02@gmail.com>
2023-02-04 19:24:58 -0700

    removing rounding from card

--- components/Dashboard/DashboardCard.js
+++ components/Dashboard/DashboardCard.js
@@ -1,7 +1,7 @@
 const DashboardCard = ({}) => {
   return (
     <>
-      <div className="card w-72 h-72 bg-white shadow-xl mt-6">
+      <div className="card w-72 h-72 bg-white shadow-xl mt-6 rounded-none">
         <div className="bg-red-500 w-full h-full flex">
           <button className="flex ml-auto text-2xl mr-2">:</button>
         </div>
