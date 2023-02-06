5ba0ebf93669362cf5fdd77e47e0d91ccddcd1ed
Amil Silahic <jinobie02@gmail.com>
2023-02-06 13:05:51 -0700

    creating more buttons for details page

--- components/CourseDetails/CourseDetails.js
+++ components/CourseDetails/CourseDetails.js
@@ -4,12 +4,14 @@ const CourseDetails = ({}) => {
       <div className="m-6 w-11/12">
         <h1 className="text-2xl text-mainText ml-12">Intro To Science</h1>
         <hr className=" mt-4 border-t border-gray-400" />
-        <div className="flex ml-auto items-center justify-center p-2 mb-6 mt-4 bg-red-50 w-32">
-          <button>+ Module</button>
+        <div className="flex ml-auto items-center justify-center gap-8  mb-6 mt-4 w-1/3">
+          <button className="bg-red-500 w-32 p-2 rounded-lg">+ Module</button>
+          <button className="bg-red-500 w-32 p-2 rounded-lg">+ Module</button>
+          <button className="bg-red-500 w-32 p-2 rounded-lg">+ Module</button>
         </div>
         <hr className="mb-6 border-t border-gray-400 " />
         <div className="flex gap-12 flex-1 w-11/12 m-auto">
-          <aside className="w-32  flex h-72 bg-red-50 items-center justify-center">
+          <aside className="w-32 flex h-72 bg-red-50 items-center justify-center">
             <nav className="">
               <ul>
                 <li>home</li>
