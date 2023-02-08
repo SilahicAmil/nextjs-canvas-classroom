39732778f41d8b17ec456819cd644869a387f059
Amil Silahic <jinobie02@gmail.com>
2023-02-08 13:10:16 -0700

    adding dropdown caret to module

--- components/CourseDetails/ModuleCard.js
+++ components/CourseDetails/ModuleCard.js
@@ -1,4 +1,5 @@
-import { AiOutlinePlus } from "react-icons/ai";
+import { AiOutlineDelete, AiOutlinePlus } from "react-icons/ai";
+
 import { HiEllipsisVertical } from "react-icons/hi2";
 import ModuleContent from "./ModuleContent";
 import { useState } from "react";
@@ -13,18 +14,24 @@ const ModuleCard = ({}) => {
   return (
     <>
       <div
-        className="w-full rounded-lg flex flex-col  mb-12 shadow-xl"
+        className="w-full rounded-lg flex flex-col  mb-12 shadow-xl hover:cursor-pointer"
         onClick={openContentHandler}
       >
         <div className="flex w-full justify-between rounded-sm h-12 bg-slate-500 items-center">
-          <h1 className="ml-8 text-2xl">Sample Module</h1>
-          <div className="flex gap-8 items-center h-full ">
+          {openContent ? (
+            <div className="rotate-180 ml-2 text-xl">^</div>
+          ) : (
+            <div className="ml-2 text-xl">&gt;</div>
+          )}
+          <h1 className="ml-8 text-2xl mr-auto">Sample Module</h1>
+          <div className="flex gap-8 items-center justify-center h-full ">
             <button className="text-xl">
               <AiOutlinePlus />
             </button>
-            <span className="ml-auto mr-6 ">
-              <HiEllipsisVertical className="text-2xl" />
-            </span>
+
+            <button className="mr-6">
+              <AiOutlineDelete className="text-xl" />
+            </button>
           </div>
         </div>
         {openContent ? <ModuleContent /> : null}
