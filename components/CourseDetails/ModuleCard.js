fa373ee4db17a293b9edf32f0f9645d236371346
Amil Silahic <jinobie02@gmail.com>
2023-02-08 12:49:58 -0700

    adding useState to open and close module content

--- components/CourseDetails/ModuleCard.js
+++ components/CourseDetails/ModuleCard.js
@@ -1,11 +1,21 @@
 import { AiOutlinePlus } from "react-icons/ai";
 import { HiEllipsisVertical } from "react-icons/hi2";
 import ModuleContent from "./ModuleContent";
+import { useState } from "react";
 
 const ModuleCard = ({}) => {
+  const [openContent, setOpenContent] = useState(true);
+
+  const openContentHandler = () => {
+    setOpenContent((prevState) => !prevState);
+  };
+
   return (
     <>
-      <div className="w-full bg-red-50 rounded-lg flex flex-col mb-12 ">
+      <div
+        className="w-full h-full bg-red-50 rounded-lg flex flex-col mb-12"
+        onClick={openContentHandler}
+      >
         <div className="flex w-full justify-between rounded-sm h-12 bg-blue-300 items-center">
           <h1 className="ml-8 text-2xl">Sample Module</h1>
           <div className="flex gap-8 items-center h-full ">
@@ -17,7 +27,7 @@ const ModuleCard = ({}) => {
             </span>
           </div>
         </div>
-        <ModuleContent />
+        {openContent ? <ModuleContent /> : null}
       </div>
     </>
   );
