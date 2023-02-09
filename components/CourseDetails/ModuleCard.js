4cdecdef970c7e48dfd7cc63a4f68e89d46972ae
Amil Silahic <jinobie02@gmail.com>
2023-02-09 12:22:34 -0700

    adding simple add module content and login apge

--- components/CourseDetails/ModuleCard.js
+++ components/CourseDetails/ModuleCard.js
@@ -1,31 +1,43 @@
 import { AiOutlineDelete, AiOutlinePlus } from "react-icons/ai";
+import { use, useState } from "react";
 
 import ModuleContent from "./ModuleContent";
-import { useState } from "react";
 
 const ModuleCard = ({}) => {
   const [openContent, setOpenContent] = useState(true);
+  const [moduleList, setModuleList] = useState([]);
 
   const openContentHandler = () => {
     setOpenContent((prevState) => !prevState);
   };
 
+  const addModuleHandler = () => {
+    setModuleList(moduleList.concat(<ModuleContent key={moduleList.length} />));
+  };
+
   return (
     <>
-      <div
-        className="w-full h-full rounded-lg flex flex-col  mb-12 shadow-xl hover:cursor-pointer"
-        onClick={openContentHandler}
-      >
+      <div className="w-full h-full rounded-lg flex flex-col  mb-12 shadow-xl ">
         <div className="flex w-full justify-between rounded-sm h-12 bg-slate-500 items-center">
           {openContent ? (
-            <div className="rotate-180 ml-2 text-xl">^</div>
+            <div
+              className="rotate-180 ml-2 text-xl cursor-pointer"
+              onClick={openContentHandler}
+            >
+              ^
+            </div>
           ) : (
-            <div className="ml-2 text-xl">&gt;</div>
+            <div
+              className="ml-2 text-xl cursor-pointer"
+              onClick={openContentHandler}
+            >
+              &gt;
+            </div>
           )}
           <h1 className="ml-8 text-2xl mr-auto">Sample Module</h1>
           <div className="flex gap-8 items-center justify-center h-full ">
             {/* when clicking plus here it should upload the files to the related db module folder name  */}
-            <button className="">
+            <button className="" onClick={addModuleHandler}>
               <AiOutlinePlus className="text-white text-xl" />
             </button>
 
@@ -35,6 +47,7 @@ const ModuleCard = ({}) => {
           </div>
         </div>
         {openContent ? <ModuleContent /> : null}
+        {moduleList}
       </div>
     </>
   );
--- pages/login/index.js
+++ pages/login/index.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { useRouter } from "next/router";
 
 const LoginPage = ({}) => {
@@ -10,20 +11,85 @@ const LoginPage = ({}) => {
 
   return (
     <>
-      <div className="flex flex-col justify-center items-center h-[100vh]">
-        <h1>Log In</h1>
-        <div className="">
-          <form onSubmit={formSubmitHandler}>
-            <div className="flex flex-col">
-              <label htmlFor="Email">Email</label>
-              <input type="email" id="Email" />
+      <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="w-full max-w-md space-y-8">
+          <div>
+            <Image
+              className="mx-auto h-12 w-auto"
+              src="/images/scholar-logo-no-bg.png"
+              alt="Scholar Logo"
+              width={1000}
+              height={1000}
+            />
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+              Sign in to your account
+            </h2>
+          </div>
+          <form className="mt-8 space-y-6" onSubmit={formSubmitHandler}>
+            <input type="hidden" name="remember" defaultValue="true" />
+            <div className="-space-y-px rounded-md shadow-sm">
+              <div>
+                <label htmlFor="email-address" className="sr-only">
+                  Email address
+                </label>
+                <input
+                  id="email-address"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  // required
+                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm bg-white "
+                  placeholder="Email address"
+                />
+              </div>
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  // required
+                  className="bg-white relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="Password"
+                />
+              </div>
             </div>
-            <div className="flex flex-col">
-              <label htmlFor="Password">Password</label>
-              <input type="passowrd" id="Password" />
+
+            <div className="flex items-center justify-between">
+              <div className="text-sm ">
+                <a
+                  href="#"
+                  className="font-medium text-indigo-600 hover:text-indigo-500"
+                >
+                  Create Account
+                </a>
+              </div>
+              <div className="text-sm ">
+                <a
+                  href="#"
+                  className="font-medium text-indigo-600 hover:text-indigo-500"
+                >
+                  Forgot your password?
+                </a>
+              </div>
             </div>
-            <div className="flex justify-center bg-red-50 text-black">
-              <button type="submit">Log In</button>
+
+            <div>
+              <button
+                type="submit"
+                className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                  {/* <LockClosedIcon
+                    className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                    aria-hidden="true"
+                  /> */}
+                </span>
+                Sign in
+              </button>
             </div>
           </form>
         </div>
