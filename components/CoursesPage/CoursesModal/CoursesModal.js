30efb4cb2857a15cef739ec701482d47d06f099f
Amil Silahic <jinobie02@gmail.com>
2023-02-15 12:16:31 -0700

    adding simple course creation form

--- components/CoursesPage/CoursesModal/CoursesModal.js
+++ components/CoursesPage/CoursesModal/CoursesModal.js
@@ -3,8 +3,7 @@ import CoursesModalContent from "./CoursesModalContent";
 const CoursesModal = ({ children }) => {
   return (
     <div className="absolute shadow-2xl m-auto bottom-0 top-0 left-0 right-0 w-2/3 h-2/3 bg-red-50 z-50 ">
-      <CoursesModalContent />
-      {children}
+      <CoursesModalContent> {children}</CoursesModalContent>
     </div>
   );
 };
--- components/CoursesPage/CoursesModal/CoursesModalContent.js
+++ components/CoursesPage/CoursesModal/CoursesModalContent.js
@@ -1,8 +1,31 @@
-const CoursesModalContent = ({}) => {
+const CoursesModalContent = ({ children }) => {
   return (
     <>
-      <div>
-        <h1>Courses Modal Content</h1>
+      <div className="h-full w-full flex flex-col">
+        <h1>Course Creation</h1>
+        <form>
+          <div>
+            <label htmlFor="name">Course Name</label>
+            <input type="text" required />
+          </div>
+          <div>
+            <label htmlFor="tag">Tag(s)</label>
+            <select id="tag">
+              <option value="Math">Select From Dropdown</option>
+              <option value="Math">Math</option>
+              <option value="Science">Science</option>
+              <option value="English">English</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="description">Description</label>
+            <textarea id="description" cols="30" rows="5" />
+          </div>
+          <div>
+            <button type="submit">Create</button>
+          </div>
+        </form>
+        {children}
       </div>
     </>
   );
