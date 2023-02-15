f9c9fe371d3bc15940793faf3897eb2d9032a76b
Amil Silahic <jinobie02@gmail.com>
2023-02-15 11:00:36 -0700

    setting default login to teacher view

--- components/Login/Login.js
+++ components/Login/Login.js
@@ -37,6 +37,7 @@ const Login = ({}) => {
                 type="email"
                 autoComplete="email"
                 // required
+                value="admin@admin.com"
                 className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm bg-white "
                 placeholder="Email address"
               />
@@ -51,6 +52,7 @@ const Login = ({}) => {
                 type="password"
                 autoComplete="current-password"
                 // required
+                value="admin"
                 className="bg-white relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                 placeholder="Password"
               />
