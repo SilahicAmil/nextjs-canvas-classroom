ec21a6c14e29b817d45ca8a42854f7d96be16c48
Amil Silahic <jinobie02@gmail.com>
2023-02-15 11:55:06 -0700

    adding course modal contentn

--- components/CoursesPage/CoursesModal/CoursesModal.js
+++ components/CoursesPage/CoursesModal/CoursesModal.js
@@ -1,7 +1,9 @@
+import CoursesModalContent from "./CoursesModalContent";
+
 const CoursesModal = ({ children }) => {
   return (
     <div className="absolute shadow-2xl m-auto bottom-0 top-0 left-0 right-0 w-2/3 h-2/3 bg-red-50 z-50 ">
-      <h1>Hello</h1>
+      <CoursesModalContent />
       {children}
     </div>
   );
---
+++ components/CoursesPage/CoursesModal/CoursesModalContent.js
@@ -0,0 +1,11 @@
+const CoursesModalContent = ({}) => {
+  return (
+    <>
+      <div>
+        <h1>Courses Modal Content</h1>
+      </div>
+    </>
+  );
+};
+
+export default CoursesModalContent;
