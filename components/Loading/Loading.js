eed118e0e19c4d1fcf39b6b0f63cd0bf6e338ccb
Amil Silahic <jinobie02@gmail.com>
2023-02-15 19:41:06 -0700

    adding loading state to app

---
+++ components/Loading/Loading.js
@@ -0,0 +1,12 @@
+const Loading = () => {
+  return (
+    <div className="flex flex-col gap-8 h-full w-full  justify-center items-center">
+      <div className="flex flex-col justify-center mt-48 items-center animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900">
+        &gt;
+      </div>
+      Loading ...
+    </div>
+  );
+};
+
+export default Loading;
--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,48 @@
 import "@/styles/globals.css";
 
+import { useEffect, useState } from "react";
+
 import Layout from "@/components/Layout/Layout";
+import Loading from "@/components/Loading/Loading";
+import Router from "next/router";
 
 export default function App({ Component, pageProps }) {
+  const [isLoading, setIsLoading] = useState(null);
+
+  useEffect(() => {
+    const start = () => {
+      setIsLoading(true);
+    };
+
+    const end = () => {
+      setIsLoading(false);
+    };
+
+    Router.events.on("routeChangeStart", start);
+    Router.events.on("routeChangeComplete", end);
+    Router.events.on("routeChangeError", end);
+
+    return () => {
+      Router.events.off("routeChangeStart", start);
+      Router.events.off("routeChangeComplete", end);
+      Router.events.off("routeChangeError", end);
+    };
+  }, []);
+
   switch (Component.displayName) {
     case "LoginPage":
       return <Component {...pageProps} />;
 
-    default:
-      return (
+    default: {
+      return isLoading ? (
+        <Layout>
+          <Loading />
+        </Layout>
+      ) : (
         <Layout>
           <Component {...pageProps} />
         </Layout>
       );
+    }
   }
 }
