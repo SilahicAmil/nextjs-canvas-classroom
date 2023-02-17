dab61108533f98bd1889398ff5b8354c79e9d82e
Amil Silahic <jinobie02@gmail.com>
2023-02-17 12:26:48 -0700

    updating misc styling for logn

--- components/Login/Login.js
+++ components/Login/Login.js
@@ -11,7 +11,7 @@ const Login = ({}) => {
 
   return (
     <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-      <div className="w-full max-w-md space-y-8">
+      <div className="w-full max-w-md space-y-8 ">
         <div>
           <Image
             className="mx-auto h-12 w-auto"
--- pages/login/index.js
+++ pages/login/index.js
@@ -3,7 +3,9 @@ import Login from "@/components/Login/Login";
 const LoginPage = ({}) => {
   return (
     <>
-      <Login />
+      <div>
+        <Login />
+      </div>
     </>
   );
 };
