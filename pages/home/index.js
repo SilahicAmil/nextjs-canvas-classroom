7a253d47ec62a9ca3db4d460e0d2184f2e5768fc
Amil Silahic <jinobie02@gmail.com>
2023-02-17 12:17:09 -0700

    adding comments for landing page

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,6 +1,9 @@
 const HomeLandingPage = () => {
   return (
     <>
+      {/* add like a typical landing page */}
+      {/* something similiar to this */}
+      {/* https://www.frontendmentor.io/challenges/meet-landing-page-rbTDS6OUR */}
       <div>
         <h1>Landing Page</h1>
       </div>
