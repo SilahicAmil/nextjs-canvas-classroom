94711cc279a0f2ec189f7586d9573544be42f02d
Amil Silahic <jinobie02@gmail.com>
2023-02-20 11:11:18 -0700

    adding comments

--- pages/404.js
+++ pages/404.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 const ErrorPage = () => {
   return (
     <>
-      <div className="flex justify-center w-screen h-screen bg-gradient-to-r from-sky-600 to-sky-300 items-center">
+      <div className="flex justify-center w-screen h-screen bg-gradient-to-r from-sky-600 to-sky-300 items-center font-lato">
         <div className="px-40 py-20 bg-white rounded-md shadow-xl">
           <div className="flex flex-col items-center">
             <h1 className="font-bold text-blue-600 text-9xl">404</h1>
@@ -15,10 +15,10 @@ const ErrorPage = () => {
             <p className="mb-8 text-center text-gray-500 md:text-lg">
               The page you’re looking for doesn’t exist.
             </p>
-
+            {/* if logged in display this stuff ELSE just take back to "/" */}
             <Link
               href="/dashboard"
-              className="px-6 py-2 text-sm font-semibold text-blue-800 bg-blue-100"
+              className="px-6 py-2 text-lg font-bold tracking-widest text-black bg-blue-300"
             >
               Go home
             </Link>
