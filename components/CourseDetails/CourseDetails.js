5b2de5f1e43d908c117be8ff02ca824b4eb07fc6
Amil Silahic <jinobie02@gmail.com>
2023-02-25 17:49:08 -0700

    more stlying updates to the card on the right

--- components/CourseDetails/CourseDetails.js
+++ components/CourseDetails/CourseDetails.js
@@ -43,9 +43,9 @@ const CourseDetails = ({ courseData, onAddModule }) => {
             })}
           </div>
 
-          <div className="flex flex-col h-full pb-4 flex-3 items-center gap-4 bg-slate-400 w-72 text-xl">
-            <h1 className="mt-4">Quote of the Day</h1>
-            <div className="w-3/4 tracking-wider h-full">
+          <div className="flex flex-col h-full pb-4 flex-3 items-center gap-4 rounded-md bg-slate-500 w-72 text-xl">
+            <h1 className="mt-4 text-gray-200">Quote of the Day</h1>
+            <div className="w-3/4 tracking-wider h-full text-gray-200">
               <p>
                 Nothing is more pathetic than people who run around in circles,
                 ‘delving into the things that lie beneath,’ and conducting
