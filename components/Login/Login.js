bbfe5a634d02a316b5c10af37040b37ca9078a0d
Amil Silahic <jinobie02@gmail.com>
2023-02-25 17:52:43 -0700

    updating the login page to get rid of erorrs

--- components/Login/Login.js
+++ components/Login/Login.js
@@ -37,7 +37,7 @@ const Login = ({}) => {
                 type="email"
                 autoComplete="email"
                 // required
-                value="admin@admin.com"
+
                 className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm bg-white "
                 placeholder="Email address"
               />
@@ -52,7 +52,7 @@ const Login = ({}) => {
                 type="password"
                 autoComplete="current-password"
                 // required
-                value="admin"
+
                 className="bg-white relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                 placeholder="Password"
               />
@@ -83,13 +83,7 @@ const Login = ({}) => {
               type="submit"
               className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
             >
-              <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                {/* <LockClosedIcon
-                    className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-                    aria-hidden="true"
-                  /> */}
-              </span>
-              Sign in
+              Sign In
             </button>
           </div>
         </form>
