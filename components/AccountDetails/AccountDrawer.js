439e55b79770280c0a06057844812b083bed3e59
Amil Silahic <jinobie02@gmail.com>
2023-03-01 14:26:15 -0700

    idk

--- components/AccountDetails/AccountDrawer.js
+++ components/AccountDetails/AccountDrawer.js
@@ -21,6 +21,7 @@ const AccountDrawer = ({}) => {
                 square={false}
                 colors={["#92A1C6", "#146A7C", "#F0AB3D", "#C271B4", "#C20D90"]}
               />
+
               <p>Amil Silahic</p>
               <button type="button" onClick={logoutHandler}>
                 Logout
