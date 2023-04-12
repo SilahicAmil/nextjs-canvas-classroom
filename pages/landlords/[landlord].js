9f14f374fa44be54c2229d56d7196f89d2669d05
Amil Silahic <jinobie02@gmail.com>
2023-04-12 14:01:05 -0600

    creating base files for /landlords and [landlord] pages for later on

---
+++ pages/landlords/[landlord].js
@@ -0,0 +1,11 @@
+const LandlordDetailsPage = () => {
+  return (
+    <>
+      <div>
+        <h1>Landlord Details Page</h1>
+      </div>
+    </>
+  );
+};
+
+export default LandlordDetailsPage;
---
+++ pages/landlords/index.js
@@ -0,0 +1,11 @@
+const LandlordsPage = () => {
+  return (
+    <>
+      <div>
+        <h1>Landlord Page</h1>
+      </div>
+    </>
+  );
+};
+
+export default LandlordsPage;
