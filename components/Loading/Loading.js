a2b91f869288e22fa362fcccd670c8247d717525
Amil Silahic <jinobie02@gmail.com>
2023-05-14 18:58:47 -0600

    comment

--- components/Loading/Loading.js
+++ components/Loading/Loading.js
@@ -1,4 +1,5 @@
 const Loading = () => {
+  // change loading spinner to react-loading-skeleton/spinner
   return (
     <div className="flex flex-col gap-8 h-full w-full  justify-center items-center">
       <div className="flex flex-col justify-center mt-48 items-center animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900">
